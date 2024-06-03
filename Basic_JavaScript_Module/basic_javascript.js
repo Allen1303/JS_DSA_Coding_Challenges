00c01debc50f157625c864cfa5303c0a265cbfe4
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2024-06-03 10:18:49 -0500

    use recursion by creating a function that return a hello world regrless of any argurments passed to the function

---
+++ Basic_JavaScript_Module/basic_javascript.js
@@ -0,0 +1,42 @@
+"use strict";
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+/**
+ *
+ * @return {Function}
+ */
+//SOLUTION
+function createHelloWorld() {
+  return function (arg) {
+    return "Hello World";
+  };
+}
+const func = createHelloWorld();
+func();
+/////////////////////////////////////////////////////////////////////////////////////////
+/*Assign the following three lines of text into the single variable myStr using escape sequences.
+FirstLine  
+\SecondLin
+ThirdLine
+You will need to use escape sequences to insert special characters correctly. You will also need to follow the spacing as it looks above, with no spaces between escape sequences or words.
+
+Note: The indentation for SecondLine is achieved with the tab escape character, not spaces.*/
+//SOLOUTION
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+//////////////////////////////////////////////////////////////////////////////////////
+/**
+ convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased: "How Can Mirrors Be Real If Our Eyes Aren't Real"
+ */
+const jadenQoute = "How can mirrors be real if our eyes aren't real";
+String.prototype.toJadenCase = function () {
+  // Splits this string into words using split(" ") and assigns them to words array.
+  const words = this.split(" ");
+  // Splits string into words, maps through them, capitalizes first letter using charAt(0).toUpperCase() and joins the rest with slice(1), creates Jaden Case.
+  return words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+};
+console.log(jadenQoute.toJadenCase()); // log the result
+////////////////////////////////////////////////////////////////////////////////////////////////
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# DSA_Coding_Challenges
+# JavaScript Data Structures & Algorithms Challenges
  Coding Challenges from FreeCodeCamp DSA Course
+
+ ## Table Of Contente
+ + __Basic JavaScript__
+
+ ### Basic JavaScript Module Summary
+
+JavaScript is a __scripting__ language you can use to make web pages __interactive__. It is one of the core technologies of the web, along with `HTML` and `CSS`, and is supported by all modern browsers.
+
+In this course, you'll learn fundamental programming concepts in JavaScript by completing 113 challenges. we will start with basic __data structures__ like `numbers` and `strings`. Then you'll learn to work with `arrays`, `objects`, `functions`, `loops`, `if/else statements`, and more.
