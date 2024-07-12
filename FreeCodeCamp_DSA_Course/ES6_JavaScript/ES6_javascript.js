5768e9c356936fe6f5f84828640d30754797dfba
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2024-07-12 18:34:50 -0500

    Update ES6_javascript.js

--- FreeCodeCamp_DSA_Course/ES6_JavaScript/ES6_javascript.js
+++ FreeCodeCamp_DSA_Course/ES6_JavaScript/ES6_javascript.js
@@ -318,3 +318,4 @@ function removeFirstTwo(list) {
 }
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sourceWithoutFirstTwo = removeFirstTwo(source);
+//////////////////////////////////////////////////////////////////////////////////////////
