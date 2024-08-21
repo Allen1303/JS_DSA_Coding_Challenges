73f6c093ab7a4ab97fae458e08b76de7672b3249
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2024-08-21 11:29:06 -0500

    Implemented regex for non-whitespace characters (\S), quantifiers for specifying match counts ({n,m}), and case-insensitive matching (i).

--- FreeCodeCamp_DSA_Course/Regular_Expressions/regex.js
+++ FreeCodeCamp_DSA_Course/Regular_Expressions/regex.js
@@ -494,6 +494,67 @@ let sample = "Whitespace is important in separating words";
 let countWhiteSpace = /\s/g;
 let result21 = sample.match(countWhiteSpace);
 ////////////////////////////////////////////////////////////////////////////////
-/**
- * @param
+/** Aug 21 2024
+ * @param {Match Non-Whitespace Characters}
+You learned about searching for whitespace using \s, with a lowercase s. You can also search for everything except whitespace.
+
+Search for non-whitespace using \S, which is an uppercase s. This pattern will not match whitespace, carriage return, tab, form feed, and new line characters. You can think of it being similar to the character class [^ \r\t\f\n\v].
+
+let whiteSpace = "Whitespace. Whitespace everywhere!"
+let nonSpaceRegex = /\S/g;
+whiteSpace.match(nonSpaceRegex).length;
+The value returned by the .length method would be 32.
+@param CHALLENGE
+Change the regex countNonWhiteSpace to look for multiple non-whitespace characters in a string.
  */
+let sample1 = "Whitespace is important in separating words";
+//Solution
+let countNonWhiteSpace = /\S/g;
+let result22 = sample1.match(countNonWhiteSpace);
+
+/**
+ * @param {Specify Upper and Lower Number of Matches}
+Recall that you use the plus sign + to look for one or more characters and the asterisk * to look for zero or more characters. These are convenient but sometimes you want to match a certain range of patterns.
+
+You can specify the lower and upper number of patterns with quantity specifiers. Quantity specifiers are used with curly brackets ({ and }). You put two numbers between the curly brackets - for the lower and upper number of patterns.
+
+For example, to match only the letter a appearing between 3 and 5 times in the string ah, your regex would be /a{3,5}h/.
+
+let A4 = "aaaah";
+let A2 = "aah";
+let multipleA = /a{3,5}h/;
+multipleA.test(A4);
+multipleA.test(A2);
+The first test call would return true, while the second would return false.
+@param CHALLENGE
+Change the regex ohRegex to match the entire phrase Oh no only when it has 3 to 6 letter h's.
+ */
+
+let ohStr = "Ohhh no";
+//Solution
+let ohRegex = /Oh{3,6}\sno/; 
+let result23 = ohRegex.test(ohStr);
+
+/**
+ * @param {Specify Only the Lower Number of Matches}
+You can specify the lower and upper number of patterns with quantity specifiers using curly brackets. Sometimes you only want to specify the lower number of patterns with no upper limit.
+
+To only specify the lower number of patterns, keep the first number followed by a comma.
+
+For example, to match only the string hah with the letter a appearing at least 3 times, your regex would be /ha{3,}h/.
+
+let A4 = "haaaah";
+let A2 = "haah";
+let A100 = "h" + "a".repeat(100) + "h";
+let multipleA = /ha{3,}h/;
+multipleA.test(A4);
+multipleA.test(A2);
+multipleA.test(A100);
+In order, the three test calls would return true, false, and true.
+@param CHALLENGE
+Change the regex haRegex to match the word Hazzah only when it has four or more letter z's.
+ */
+let haStr = "Hazzzzah";
+//Solution
+let haRegex = /Haz{4,}ah/;
+let result24 = haRegex.test(haStr);
