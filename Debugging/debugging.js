233c759336523bc9b1268dbafc680b1d9a5e4f3c
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2024-09-03 08:17:10 -0500

    Completed debugging exercises:
    Off-by-one errors: Addressed issues related to incorrect array indexing.
    Reinitialized variables: Corrected errors caused by reinitializing variables within loops.

--- Debugging/debugging.js
+++ Debugging/debugging.js
@@ -141,7 +141,8 @@ let x = 7;
 let y = 9;
 let result = "to come";
 //Solution
-if(x == y) {  // double equal operator used
+if (x == y) {
+  // double equal operator used
   result = "Equal!";
 } else {
   result = "Not equal!";
@@ -217,9 +218,70 @@ function countToFive() {
   let firstFive = "12345";
   let len = firstFive.length;
   // Only change code below this line
-  for (let i = 0; i < len; i++) { //start the count at 0 and used < symbol for the condition
-  // Only change code above this line
+  for (let i = 0; i < len; i++) {
+    //start the count at 0 and used < symbol for the condition
+    // Only change code above this line
     console.log(firstFive[i]);
   }
 }
 countToFive();
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @param {Use Caution When Reinitializing Variables Inside a Loop}
+Sometimes it's necessary to save information, increment counters, or re-set variables within a loop. A potential issue is when variables either should be reinitialized, and aren't, or vice versa. This is particularly dangerous if you accidentally reset the variable being used for the terminal condition, causing an infinite loop.
+
+Printing variable values with each cycle of your loop by using console.log() can uncover buggy behavior related to resetting, or failing to reset a variable.
+@param CHALLENGE
+The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes. Unfortunately, it's not producing the expected output because the row variable isn't being reinitialized (set back to an empty array) in the outer loop. Fix the code so it returns a correct 3x2 array of zeroes, which looks like [[0, 0], [0, 0], [0, 0]].
+
+ */
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  // let row = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+    let row = []; /* row has been declared inside the outer loop. 
+Now a new row will be initialised during each iteration of the outer loop allowing 
+for the desired matrix. */
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row.push(0);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+/**
+ * @param Prevent Infinite Loops with a Valid Terminal Condition
+The final topic is the dreaded infinite loop. Loops are great tools when you need your program to run a code block a certain number of times or until a condition is met, but they need a terminal condition that ends the looping. Infinite loops are likely to freeze or crash the browser, and cause general program execution mayhem, which no one wants.
+
+There was an example of an infinite loop in the introduction to this section - it had no terminal condition to break out of the while loop inside loopy(). Do NOT call this function!
+
+function loopy() {
+  while(true) {
+    console.log("Hello, world!");
+  }
+}
+It's the programmer's job to ensure that the terminal condition, which tells the program when to break out of the loop code, is eventually reached. One error is incrementing or decrementing a counter variable in the wrong direction from the terminal condition. Another one is accidentally resetting a counter or index variable within the loop code, instead of incrementing or decrementing it.
+
+@param CHALLENGE
+The myFunc() function contains an infinite loop because the terminal condition i != 4 will never evaluate to false (and break the looping) - i will increment by 2 each pass, and jump right over 4 since i is odd to start. Fix the comparison operator in the terminal condition so the loop only runs for i less than or equal to 4.
+
+//  */
+// function myFunc() {
+//   for (let i = 1; i != 4; i += 2) {
+//     console.log("Still going!");
+//   }
+// }
+function myFunc() {
+  for (let i = 1; i != 4; i += 2) { // Solution change the Not (!) operator to less than equal to operator (<=)
+    console.log("Still going!");
+  }
+}
